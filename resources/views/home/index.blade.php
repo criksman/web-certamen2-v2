f2343cf3852d6ae08bf62f4dc580559fb3b75bab
Daniel Saldivar <danielsaldivargonzalez@gmail.com>
2023-06-08 21:01:12 -0400

    home y navbar

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,4 +2,16 @@
 
 @section('main-content')
 
+<div class="container"style="margin-top: 80px">
+    <div class="row justify-content-center">
+    <div class="col-lg-7 py-5">
+        <div class="card mb-2" style="background-color: #003389">
+    <div class="card-body d-flex justify-content-center">
+        <h1 class="card-title text-center"style="color: white;">Certamen°2</h1>
+    </div>
+    <h2 class="text-center" style="color: white;padding: 10px;">Programacion orientada a la web</h2>
+</div>
+
+
+
 @endsection
